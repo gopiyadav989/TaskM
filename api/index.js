9b064a2f1f050a53301cc52aa353e71d0bc6dbfa
gopiyadav989 <gopiyadu17@.com>
2024-12-31 19:07:17 +0530

    pre install

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from "express";
 import dbConnection from "./utils/index.js";
 import routes from "./routes/index.js";
+import path from 'path';
 
 dotenv.config();
 dbConnection();
@@ -17,12 +18,19 @@ app.use(cors({
     credentials: true,
 }));
 
+const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());  
 app.use('/api', routes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
